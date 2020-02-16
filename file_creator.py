77890b8486d946d5864abc6c355678d7a82c0887
iHoudi <60161540+iHoudi@users.noreply.github.com>
2020-02-16 13:18:44 -0500

    Updated for version 0.2.1
    0.2.1 (02/16/2020): +Fixed issues with handling invalid inputs - Updates:
      1. 'main.py' has been reworked to handle invalid inputs.
      2. 'file_creator.py' received changes to grammar for "fields."
      3. Current issues or bugs - 
        1. There is no validation that the file supplied is the one expected.

--- file_creator.py
+++ file_creator.py
@@ -13,27 +13,33 @@ def make_json():
     statement_info = [repo]
     wrapper = {"statement": statement_info}
 
-    for i in range(0, 8):
-        if i <= 2:
-            response = input(fields[i])
-            repo.update({keys[i]: response})
-        elif i > 2 and i < 7:
-            print(i)
-            response = int(input(fields[i]))
-            repo.update({keys[i]: response})
-        elif i == 7:
-            old_bal = int(repo.get('old_bal'))
-            deposit = int(repo.get('deposit'))
-            withdraw = int(repo.get('withdraw'))
+    try:
+        for i in range(0, 8):
+            if i <= 2:
+                response = input(fields[i])
+                repo.update({keys[i]: response})
+            elif i > 2 and i < 7:
+                response = int(input(fields[i]))
+                repo.update({keys[i]: response})
+            elif i == 7:
+                old_bal = int(repo.get('old_bal'))
+                deposit = int(repo.get('deposit'))
+                withdraw = int(repo.get('withdraw'))
 
-            updated_bal = (old_bal + deposit) - withdraw
-            repo.update({keys[7]: updated_bal})
+                updated_bal = (old_bal + deposit) - withdraw
+                repo.update({keys[7]: updated_bal})
 
-    with open('statments.json', 'w') as json_file:
-        json.dump(wrapper, json_file, indent=4, sort_keys=False)
+        file_name = input("current month/year>>") + "_statement.json"
 
-    for response in range(0, 7):
-        # print(responses[response])
-        print(keys[response], repo.get(keys[response]))
+        with open(file_name, 'w') as json_file:
+            json.dump(wrapper, json_file, indent=4, sort_keys=False)
 
-    print("Creating new file...")
+        # This code is for debugging if you so choose.
+        # for response in range(0, 7):
+        #     # print(responses[response])
+        #     print(keys[response], repo.get(keys[response]))
+
+        print("Creating new file...")
+
+    except(Exception):
+        print("Error creating JSON")
--- main.py
+++ main.py
@@ -14,20 +14,43 @@ class Pynance:
 
     def main(self):
 
-        while True:
-            print(
-                "Mode selection:\n 0 = load existing file,\n 1 = Create new file,\n 2 = Exit out of Pynance")
+        # Keeps track of how many times input has been provided. IF it reaches '2' the program will exit
+        n = 0
 
-            self.mode = int(input("Mode: "))
-            if self.mode == 0:
-                file_selector()
-            elif self.mode == 1:
-                make_json()
-            elif self.mode == 2:
-                exit()
-            else:
-                print("Issue loading data or invalid mode")
-            return False
+        # Provides the loop for the program
+        while True:
+            # Attempts to take user input and call the appropriate method
+            try:
+                print(
+                    "Mode selection:\n 0 = load existing file,\n 1 = Create new file,\n 2 = Exit out of Pynance")
+                self.mode = int(input("Mode: "))
+
+                if self.mode == 0:
+                    file_selector()
+
+                elif self.mode == 1:
+                    make_json()
+
+                elif self.mode == 2:
+                    exit()
+
+                elif n == 2:
+                    # We do 'n+1' to display the amount of invalid entries with a base of '1' instead of '0'
+                    print(f"{n+1} invalid entries... Exiting program.")
+                    exit()
+
+                else:
+                    print("Mode not listed\n")
+                    n += 1
+
+            except(ValueError):
+                if n == 2:
+                    # Refer to the comment on line 38
+                    print(f"{n+1} invalid entries... Exiting program.")
+                    exit()
+
+                n += 1
+                print("Invalid Input\n")
 
 
 Pynance().main()
